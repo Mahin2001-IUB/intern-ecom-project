efcef7ee66eb4af37d41610ce2fb903fdf1b3382
Muhaimin2030029 <muhaimin.rahman123@gmail.com>
2026-05-31 00:30:31 +0600

    user order page created and connected to the database

--- app/(public)/orders/page.jsx
+++ app/(public)/orders/page.jsx
@@ -1,46 +1,76 @@
-'use client'
-import PageTitle from "@/components/PageTitle"
+"use client";
+
+import PageTitle from "@/components/PageTitle";
 import { useEffect, useState } from "react";
 import OrderItem from "@/components/OrderItem";
-import { orderDummyData } from "@/assets/assets";
+import Loading from "@/components/Loading";
+import { toast } from "react-hot-toast";
 
 export default function Orders() {
+  const [orders, setOrders] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-    const [orders, setOrders] = useState([]);
+  const fetchOrders = async () => {
+    try {
+      const res = await fetch("/api/orders", {
+        cache: "no-store",
+      });
 
-    useEffect(() => {
-        setOrders(orderDummyData)
-    }, []);
+      const data = await res.json();
 
-    return (
-        <div className="min-h-[70vh] mx-6">
-            {orders.length > 0 ? (
-                (
-                    <div className="my-20 max-w-7xl mx-auto">
-                        <PageTitle heading="My Orders" text={`Showing total ${orders.length} orders`} linkText={'Go to home'} />
+      if (!data.success) {
+        throw new Error(data.message || "Failed to fetch orders");
+      }
 
-                        <table className="w-full max-w-5xl text-slate-500 table-auto border-separate border-spacing-y-12 border-spacing-x-4">
-                            <thead>
-                                <tr className="max-sm:text-sm text-slate-600 max-md:hidden">
-                                    <th className="text-left">Product</th>
-                                    <th className="text-center">Total Price</th>
-                                    <th className="text-left">Address</th>
-                                    <th className="text-left">Status</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {orders.map((order) => (
-                                    <OrderItem order={order} key={order.id} />
-                                ))}
-                            </tbody>
-                        </table>
-                    </div>
-                )
-            ) : (
-                <div className="min-h-[80vh] mx-6 flex items-center justify-center text-slate-400">
-                    <h1 className="text-2xl sm:text-4xl font-semibold">You have no orders</h1>
-                </div>
-            )}
+      setOrders(data.orders);
+    } catch (error) {
+      console.error("FETCH_USER_ORDERS_ERROR:", error);
+      toast.error(error.message || "Failed to fetch orders");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchOrders();
+  }, []);
+
+  if (loading) return <Loading />;
+
+  return (
+    <div className="min-h-[70vh] mx-6">
+      {orders.length > 0 ? (
+        <div className="my-20 max-w-7xl mx-auto">
+          <PageTitle
+            heading="My Orders"
+            text={`Showing total ${orders.length} orders`}
+            linkText="Go to home"
+          />
+
+          <table className="w-full max-w-5xl text-slate-500 table-auto border-separate border-spacing-y-12 border-spacing-x-4">
+            <thead>
+              <tr className="max-sm:text-sm text-slate-600 max-md:hidden">
+                <th className="text-left">Product</th>
+                <th className="text-center">Total Price</th>
+                <th className="text-left">Address</th>
+                <th className="text-left">Status</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              {orders.map((order) => (
+                <OrderItem order={order} key={order.id} />
+              ))}
+            </tbody>
+          </table>
         </div>
-    )
+      ) : (
+        <div className="min-h-[80vh] mx-6 flex items-center justify-center text-slate-400">
+          <h1 className="text-2xl sm:text-4xl font-semibold">
+            You have no orders
+          </h1>
+        </div>
+      )}
+    </div>
+  );
 }
